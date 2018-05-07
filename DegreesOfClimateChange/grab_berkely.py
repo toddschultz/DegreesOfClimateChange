43c21af7d89849fcabea5e467fe8773da32ff5bb
Abhishek Anand <anand1@uw.edu>
2018-05-06 18:50:41 -0700

    Adding grab_berkely.py

---
+++ DegreesOfClimateChange/grab_berkely.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+grab_berekely retrieves global average temperatures from berkeley.
+
+
+
+Written by Abhishek Anand
+2018
+"""
+
+import datetime
+import pandas as pd
+import requests

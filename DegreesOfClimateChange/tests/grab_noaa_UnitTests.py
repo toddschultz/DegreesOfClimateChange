8ace53106bcbd407853d7f9b65088be5afd97d74
Todd Schultz <toddschultz@gmail.com>
2018-05-09 22:06:06 -0700

    grab_noaa unit test partial complete

--- DegreesOfClimateChange/tests/grab_noaa_UnitTests.py
+++ DegreesOfClimateChange/tests/grab_noaa_UnitTests.py
@@ -12,12 +12,14 @@ Excceptions thrown for all failed tests.
 Written by Todd Schultz
 2018
 """
-import unittest
-import os
+
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 import grab_noaa
+import unittest
 
 
-dfnoaa = grab_noaa.grab_temperatures_noaa();
+dfnoaa = grab_noaa.grab_temperatures_noaa()
 
 
 class grab_naoo_UnitTests(unittest.TestCase):
@@ -26,15 +28,14 @@ class grab_naoo_UnitTests(unittest.TestCase):
     def test_column_names(self):
         """Test for exact 2 required column names.
         The required names are Date and Tanomaly_C"""
-        df_test = grab_noaa.grab_temperatures_noaa()
         # if we test for the number of columns to match the number of exact
         #    column names, and that we have at least one column of each of the
         #    required names then we can conclude that we have only the exact
         #    columns required
         pass_test = True
         known_names = ('Date', 'Tanomaly_C')
-        pass_test = pass_test & (df_test.shape[1] == len(known_names))
-        col_names = df_test.columns
+        pass_test = pass_test & (dfnoaa.shape[1] == len(known_names))
+        col_names = dfoaa.columns
 
         def is_valid_column(cnames, testcname):
             """Test if column names are a member of the required names."""
@@ -48,12 +49,21 @@ class grab_naoo_UnitTests(unittest.TestCase):
                 col_names, y_known_column[1]))
         self.assertTrue(pass_test)
 
-    def test_Date_datatypes(self):
+    def test_datatypes(self):
         """Test that column Date is a string data type
         A string in Pandas is actually an object."""
-        #dfnoaa = grab_noaa.grab_temperatures_noaa();
+        self.assertTrue(dfnoaa["Date"].dtypes == 'O')
+        self.assertTrue(dfnoaa["Tanomaly_C"].dtype == 'float')
+
+    def test_Monthly_data(self):
+        """Test that there is a data point for every month for the dates
+        returned in the data frame."""
+        start_date = datetime.strptime(dfnoaa["Date"].iloc[0], '%Y-%m-%d')
+        end_date = datetime.strptime(dfnoaa["Date"].iloc[-1], '%Y-%m-%d')
+        r = relativedelta(end_date, start_date)
+        n_months = 12*r.years + r.months + 1
         
-        self.assertTrue(dfnoaa.dtypes == 'O')
+        self.assertTrue(n_months == dfnoaa.shape[0])
 
 
 if __name__ == '__main__':

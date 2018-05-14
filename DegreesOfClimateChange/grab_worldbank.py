1463bfdd2fc21c12cd0ffaa7f896078372c7a02e
Rahul Birmiwal <rrbirmiw@uw.edu?git config --global user.email rrbirmiw@uw.edu>
2018-05-14 03:34:16 -0700

    Working on grouping_by country, not finished

--- DegreesOfClimateChange/grab_worldbank.py
+++ DegreesOfClimateChange/grab_worldbank.py
@@ -26,14 +26,6 @@ def grab_worldbank(select_countries=None, start_date = None, end_date = None):
 
     """
 
-    """Argument/type checking"""
-    if (  (select_countries is not None and not isinstance(select_countries,list)) or
-          (start_date is not None and not isinstance(start_date,int)) or
-           (end_date is not None and not isinstance(end_date, int))
-        ) :
-        raise ValueError("Error: Arguments are of incorrect datatype!")
-
-
     if (end_date is not None and end_date > 2015):
         raise ValueError("Error: Starting date cannot exceed 2015")
 
@@ -53,20 +45,9 @@ def grab_worldbank(select_countries=None, start_date = None, end_date = None):
     codes_list = np.array(iso_codes_df['CODE'])
     country_list = np.array(iso_codes_df['COUNTRY'])
 
-
-    """Run data collections"""
-    if (select_countries is not None):
-        try:
-            code_country_pairs = [(country_list[k],codes_list[k]) for k in range(0,len(codes_list)) if country_list[k] in select_countries]
-            #country_codes = iso_codes_df['CODE']
-        except Exception as e:
-            print(e)
-            sys.exit(0)
-    else:
-        code_country_pairs = [(country_list[k],codes_list[k]) for k in range(0,len(codes_list))]
-    data_len = len(code_country_pairs)
+    code_country_pairs = [(country_list[k],codes_list[k]) for k in range(0,len(codes_list))]
     dataset = {}
-    for k in range(0,data_len):
+    for k in range(0,len(code_country_pairs)):
         code = code_country_pairs[k][1]
         try:
             dataset[code]=climate_api.get_instrumental(data_type='tas',interval='year',locations=[code])
@@ -77,50 +58,25 @@ def grab_worldbank(select_countries=None, start_date = None, end_date = None):
             continue
 
     """Create pandas dataframe of the data"""
-
-    df = pd.DataFrame(columns=['Country','Code','Year','Temperature'])
+    df = pd.DataFrame(columns=['Country','Year','Temperature'])
 
     count = 0
-    for k in range(0,data_len):
+    for k in range(0,len(code_country_pairs)):
         c_name,c_code = code_country_pairs[k][0], code_country_pairs[k][1]
         if (c_code not in err_dict):
             country_data = dataset[c_code].as_dict()[c_code]
             for year_key in country_data.keys():
                 if (int(year_key) >= start_date and int(year_key) <= end_date):
-                    df.loc[count] = [c_name, c_code,year_key, country_data[year_key]]
+                    df.loc[count] = [c_name,int(year_key), country_data[year_key]]
                     count += 1
-    return df
+
+
+
+    temp_and_year_df = df.groupby(df['Year'])
+    return temp_and_year_df
 
 
 def test_output_grab_worldbank():
-    """Get all data for only United States"""
-    print("Get all data for only United States")
-    select = ['United States']
-    wb_data_df = grab_worldbank(select_countries=select)
-    print(wb_data_df.head(30))
-    print("------------------------------------------------------------")
-
-    """Get all data for only United States, Burkina Faso, and Georgia"""
-    print("Get all data for only United States, Burkina Faso, and Georgia")
-    select = ['United States','Burkina Faso', 'Georgia']
-    wb_data_df = grab_worldbank(select_countries=select)
-    print(wb_data_df.head(30))
-    print("------------------------------------------------------------")
-
-    """Get only year 2000-ending date data for only United States, Burkina Faso, and Georgia"""
-    print("Get only year 2000-ending date data for only United States, Burkina Faso, and Georgia")
-    select = ['United States','Burkina Faso', 'Georgia']
-    wb_data_df = grab_worldbank(select_countries=select, start_date = 2000)
-    print(wb_data_df.head(30))
-    print("------------------------------------------------------------")
-
-    """Get only 1910-1915 date data for only United States, Burkina Faso, and Georgia"""
-    print("Get only 1910-1915 date data for only United States, Burkina Faso, and Georgia")
-    select = ['United States','Burkina Faso', 'Georgia']
-    wb_data_df = grab_worldbank(select_countries=select, start_date = 1910, end_date=1915)
-    print(wb_data_df.head(30))
-    print("------------------------------------------------------------")
-
     """Get all data for all countries"""
     print("Get all data for all countries")
     wb_data_df = grab_worldbank()

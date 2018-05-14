4aad56b173fc1f0b83d87292fe010a73aef33625
Rahul Birmiwal <rrbirmiw@uw.edu?git config --global user.email rrbirmiw@uw.edu>
2018-05-14 09:50:12 -0700

    Function now returns relation R(Year, AvgTemperature). Also modified so that doesn't require offline/saved ISO 3166 country codes file.

--- DegreesOfClimateChange/grab_worldbank.py
+++ DegreesOfClimateChange/grab_worldbank.py
@@ -4,9 +4,15 @@ import sys
 import numpy as np
 
 
+import wbpy
+import pandas as pd
+import sys
+import numpy as np
 
 
-def grab_worldbank(select_countries=None, start_date = None, end_date = None):
+
+
+def grab_worldbank(start_date = None, end_date = None):
     """
     ################ FIX THIS DOCSTRING #####################
     Note: NEED to install wbpy <------- **
@@ -15,14 +21,12 @@ def grab_worldbank(select_countries=None, start_date = None, end_date = None):
         * Our project will appropriately unittest any and all functions incorporating the wbpy library.
 
     Arguments:
-        select_countries (list): List of countries from which to obtain temperature data.
-                                Default 'None' means obtain from all countries
         start_date (int): Starting date in yyyy integer format for the historical data.
                           Default 'None' means 1901, the earliest available year on the World Bank dataset
         end_date (int): Ending date in yyyy integer format. Default 'None' means 2015.
 
     Returns:
-    - Pandas DataFrame:
+        pandas dataframe: Columns are (Year, Mean Temperature)
 
     """
 
@@ -40,11 +44,18 @@ def grab_worldbank(select_countries=None, start_date = None, end_date = None):
     """Instantiate API Interface using wbpy package"""
     climate_api = wbpy.ClimateAPI()
 
-    """Read in country codes from World Bank website"""
-    iso_codes_df = pd.read_csv('country_iso_codes.csv').dropna()
-    codes_list = np.array(iso_codes_df['CODE'])
-    country_list = np.array(iso_codes_df['COUNTRY'])
+    """Obtain ISO country codes for ALL countries in the world"""
+    iso_df = pd.read_csv('https://raw.githubusercontent.com/datasets/country-codes/master/data/country-codes.csv',
+                  delimiter=',')
+    codes_list = np.array(iso_df['ISO3166-1-Alpha-3'])#i.e. [..., DEU, GHA, GIB,...]
+    country_list = np.array(iso_df['official_name_en'])#i.e. [...Germany, Ghana, Gibraltar, ...]
 
+
+    """
+        Create a 'temporary' _dataset_ of (country, year, average annual temperature) triples.
+        To do this, we will need to use the climate_api.get_instrumental() wrapper function,
+        and store the wrapper results into a dictionary called _dataset_
+    """
     code_country_pairs = [(country_list[k],codes_list[k]) for k in range(0,len(codes_list))]
     dataset = {}
     for k in range(0,len(code_country_pairs)):
@@ -57,31 +68,51 @@ def grab_worldbank(select_countries=None, start_date = None, end_date = None):
             err_dict[code] = 1
             continue
 
-    """Create pandas dataframe of the data"""
+    """
+        Create a pandas dataframe from the dataset dictionary
+        by parsing the content of the calls to climate_api.get_instrumental(), stored in dataset
+    """
     df = pd.DataFrame(columns=['Country','Year','Temperature'])
-
     count = 0
+    #For all countries
     for k in range(0,len(code_country_pairs)):
         c_name,c_code = code_country_pairs[k][0], code_country_pairs[k][1]
+
+        #if is a valid country
         if (c_code not in err_dict):
+
+            #convert the get_instrumental() content to dictionary and access it's
+            # 'subdictionary' using the appropriate country code
             country_data = dataset[c_code].as_dict()[c_code]
+
+            #for all years that are in the valid time window
             for year_key in country_data.keys():
                 if (int(year_key) >= start_date and int(year_key) <= end_date):
+                    #Append to dataframe
                     df.loc[count] = [c_name,int(year_key), country_data[year_key]]
                     count += 1
 
+    """
+        At this point, df points to a relation df -> R(Country, Year, Temperature).
+        Since we need to return global average temperature, we compute the mean of all
+        countries per given year, and store the results in a new dataframe
+    """
 
-
-    temp_and_year_df = df.groupby(df['Year'])
+    temp_and_year_df = df.groupby(df['Year']).mean()
     return temp_and_year_df
 
 
+
+
 def test_output_grab_worldbank():
-    """Get all data for all countries"""
-    print("Get all data for all countries")
+    """Get global average temperature across all countries worldwide, for
+    1901-2015"""
+    print("Print getting data")
     wb_data_df = grab_worldbank()
-    print(wb_data_df.tail(20))
+    print("Now printing (Year, AverageTemperature) tuples ")
+    print(wb_data_df.head())
     print("------------------------------------------------------------")
+    print(wb_data_df.tail())
 
 if __name__ == '__main__':
     test_output_grab_worldbank()

3900520257f1c8e630d2abcaf4724f1882299c18
Todd Schultz <toddschultz@gmail.com>
2018-05-17 21:46:16 -0700

    cleaned up comparison notebook, worked on import for grab_worldbank in unit tests

--- DegreesOfClimateChange/tests/test_grab_worldbank.py
+++ DegreesOfClimateChange/tests/test_grab_worldbank.py
@@ -1,6 +1,7 @@
 
 import sys
-import grab_worldbank as grab_worldbank
+import DegreesOfClimateChange.grab_worldbank as grab_worldbank
+#import grab_worldbank as grab_worldbank
 import unittest
 import datetime
 import warnings
@@ -12,7 +13,8 @@ def ignore_warnings(test_func):
     def do_test(self, *args, **kwargs):
         with warnings.catch_warnings():
 
-            """File I/O is known to raise ResourceWarning in unittest so ignore it"""
+            """File I/O is known to raise ResourceWarning in
+            unittest so ignore it"""
             warnings.simplefilter("ignore", ResourceWarning)
             test_func(self, *args, **kwargs)
     return do_test
@@ -37,15 +39,15 @@ class TestGrabWorldbank(unittest.TestCase):
 
     @ignore_warnings
     def test_arguments(self):
-        self.assertRaises(ValueError, grab_worldbank.grab_worldbank, 'foo','baz')
-        self.assertRaises(ValueError, grab_worldbank.grab_worldbank, '','baz')
-        self.assertRaises(ValueError, grab_worldbank.grab_worldbank, 'foo','')
-        self.assertRaises(ValueError, grab_worldbank.grab_worldbank, '1901','2012')
+        self.assertRaises(ValueError, grab_worldbank.grab_worldbank, 'foo', 'baz')
+        self.assertRaises(ValueError, grab_worldbank.grab_worldbank, '', 'baz')
+        self.assertRaises(ValueError, grab_worldbank.grab_worldbank, 'foo', '')
+        self.assertRaises(ValueError, grab_worldbank.grab_worldbank, '1901', '2012')
         self.assertRaises(ValueError, grab_worldbank.grab_worldbank, '$&', None)
-        self.assertRaises(ValueError, grab_worldbank.grab_worldbank, None,'$&')
+        self.assertRaises(ValueError, grab_worldbank.grab_worldbank, None, '$&')
         self.assertRaises(ValueError, grab_worldbank.grab_worldbank, -TestGrabWorldbank.min_year, TestGrabWorldbank.max_year)
         self.assertRaises(ValueError, grab_worldbank.grab_worldbank, TestGrabWorldbank.min_year, -TestGrabWorldbank.max_year)
-        self.assertRaises(ValueError, grab_worldbank.grab_worldbank, 0,0)
+        self.assertRaises(ValueError, grab_worldbank.grab_worldbank, 0, 0)
         self.assertRaises(ValueError, grab_worldbank.grab_worldbank, None, datetime.datetime.now().year)
 
     def tearDown(self):

6fac4d6d9cac6624629995cbb72fa1ef5c899934
Abhishek Anand <anand1@uw.edu>
2018-05-20 22:09:39 -0700

    Cretion of file for unit test grab_berkeley.py

---
+++ DegreesOfClimateChange/test_grab_berkeley.py
@@ -0,0 +1,12 @@
+"""Unit test for grab_berkeley.py
+This Python module contains multiple unit test functions to verify the
+execution of the grab_berkeley.py module. Tests include verifying the number
+of columns, the exact column names, the data types in each column, and more.
+Excceptions thrown for all failed tests.
+(class) grab_berkeley.py
+    Python class for unit testing the Python function
+    grab_noaa() in the  grab_berkeley.py module.
+Written by Abhishek Anand 
+2018
+"""
+

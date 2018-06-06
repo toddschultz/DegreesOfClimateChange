45c63028deaccdbe01301a377daef56266208fd7
Rahul Birmiwal <rrbirmiw@uw.edu?git config --global user.email rrbirmiw@uw.edu>
2018-06-05 21:45:58 -0700

    added testing coverage

--- DegreesOfClimateChange/tests/test_plot_functions.py
+++ DegreesOfClimateChange/tests/test_plot_functions.py
@@ -30,9 +30,11 @@ Written by Rahul Birmiwal
 import sys
 sys.path.append("..") # Adds higher directory to python modules path.
 from DegreesOfClimateChange.plot_functions import plot_each_absolute_temperature
+from DegreesOfClimateChange.plot_functions import plot_co2_against_temperature
 from DegreesOfClimateChange.grab_worldbank import grab_worldbank
 from DegreesOfClimateChange.grab_berkeley import grab_berkeley
 from DegreesOfClimateChange.grab_noaa import grab_noaa
+from DegreesOfClimateChange.grab_co2_scripps import grab_scripps_co2_data
 import unittest
 import datetime
 import warnings
@@ -67,8 +69,10 @@ class TestPlotFunctions(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestPlotFunctions, self).__init__(*args, **kwargs)
         self.df_wb = grab_worldbank(2009, 2010) # arbitrary dates chosen in
-                                           # tight range for speed
-
+                                                # tight range for speed
+        self.df_noaa = grab_noaa()
+        self.db_berkeley = grab_berkeley()
+        self.df_co2 = grab_scripps_co2_data()
 
     @ignore_warnings
     def setUp(self):
@@ -76,14 +80,22 @@ class TestPlotFunctions(unittest.TestCase):
         print('In setUp()')
 
         # dataframes used in plotting
-        df_noaa = grab_noaa()
-        df_berkeley = grab_berkeley()
+        df_noaa = self.df_noaa
+        df_berkeley = self.db_berkeley
         df_worldbank = self.df_wb # using the cached dataframe
-
+        df_co2 = self.df_co2
         # Define the testing unit
         (self.ydata, self.xdata) = plot_each_absolute_temperature(df_noaa,
                                     df_berkeley, df_worldbank, do_plot=False,
                                     fig_num=None)
+        (self.ydata_wco2, self.xdata_wco2) = plot_co2_against_temperature(
+                                    df_co2, df_noaa,
+                                    df_berkeley, df_worldbank, do_plot=False,
+                                    fig_num=0)
+
+
+
+
 
     @ignore_warnings
     def test_valueError(self):
@@ -92,8 +104,24 @@ class TestPlotFunctions(unittest.TestCase):
         be a Pandas DataFrame, was not a pandas dataframe
         """
         try:
+            # Testing plot_each_absolute_temperature
             raises_error(plot_each_absolute_temperature(None, None, None,
                                             False, None))
+            raises_error(plot_each_absolute_temperature(pd.DataFrame(), None, None,
+                                            False, None))
+            raises_error(plot_each_absolute_temperature(None, pd.DataFrame(), None,
+                                            False, None))
+            raises_error(plot_each_absolute_temperature(None, None, pd.DataFrame(),
+                                            False, None))
+
+
+            # Testing plot_co2_against_temperature
+            raises_error(plot_co2_against_temperature(None, None, None, None))
+            raises_error(plot_co2_against_temperature(None, pd.DataFrame(), None, None))
+            raises_error(plot_co2_against_temperature(None, None, pd.DataFrame(), None))
+            raises_error(plot_co2_against_temperature(None, None, None, pd.DataFrame()))
+
+
         except ValueError:
             pass
         else:
@@ -122,12 +150,15 @@ class TestPlotFunctions(unittest.TestCase):
         for v in vals:
             self.assertTrue(all(isinstance(elem, int) for elem in v) or
                             all(isinstance(elem, float) for elem in v))
-    def test_x_axis(self):
-        """test_x_axis tests that the x-axes returned by
+
+
+    @ignore_warnings
+    def test_y_axis(self):
+        """test_y_axis tests that the y-axes returned by
         plot_each_absolute_temperature is a valid set of axes"""
 
-        self.assertIsInstance(self.xdata, dict)
-        keys = list(self.xdata.keys())
+        self.assertIsInstance(self.ydata, dict)
+        keys = list(self.ydata.keys())
         keys.sort()
 
         # test that the correct agency names are in the x-axis dictionary
@@ -137,29 +168,56 @@ class TestPlotFunctions(unittest.TestCase):
         self.assertTrue(keys[2] == 'WorldBank')
         self.assertTrue(len(keys) == 3)
 
-        vals = self.xdata.values()
+        vals = self.ydata.values()
         # test that each agency_name points to a list of numeric values
         # that could be used on the x-axis
         for v in vals:
             self.assertTrue(all(isinstance(elem, int) for elem in v) or
                             all(isinstance(elem, float) for elem in v))
 
-    def test_y_axis(self):
-        """test_y_axis tests that the y-axes returned by
-        plot_each_absolute_temperature is a valid set of axes"""
+    @ignore_warnings
+    def test_y_axis_co2plot(self):
+        """test_y_axis_co2plot tests that the y-axes returned by
+        plot_co2_against_temperature is a valid set of axes"""
 
-        self.assertIsInstance(self.ydata, dict)
-        keys = list(self.xdata.keys())
+        self.assertIsInstance(self.ydata_wco2, dict)
+        keys = list(self.ydata_wco2.keys())
         keys.sort()
 
         # test that the correct agency names are in the x-axis dictionary
         # and we only have our three main data sources
         self.assertTrue(keys[0] == 'Berkeley')
         self.assertTrue(keys[1] == 'NOAA')
-        self.assertTrue(keys[2] == 'WorldBank')
-        self.assertTrue(len(keys) == 3)
+        self.assertTrue(keys[1] == 'Scripps')
+        self.assertTrue(keys[3] == 'WorldBank')
+        self.assertTrue(len(keys) == 4)
 
-        vals = self.xdata.values()
+        vals = self.ydata_wco2.values()
+        # test that each agency_name points to a list of numeric values
+        # that could be used on the x-axis
+        for v in vals:
+            self.assertTrue(all(isinstance(elem, int) for elem in v) or
+                            all(isinstance(elem, float) for elem in v))
+
+
+    @ignore_warnings
+    def test_x_axis_co2plot(self):
+        """test_x_axis_co2plot tests that the x-axes returned by
+        plot_co2_against_temperature is a valid set of axes"""
+
+        self.assertIsInstance(self.xdata_wco2, dict)
+        keys = list(self.xdata_wco2.keys())
+        keys.sort()
+
+        # test that the correct agency names are in the x-axis dictionary
+        # and we only have our three main data sources
+        self.assertTrue(keys[0] == 'Berkeley')
+        self.assertTrue(keys[1] == 'NOAA')
+        self.assertTrue(keys[1] == 'Scripps')
+        self.assertTrue(keys[3] == 'WorldBank')
+        self.assertTrue(len(keys) == 4)
+
+        vals = self.xdata_wco2.values()
         # test that each agency_name points to a list of numeric values
         # that could be used on the x-axis
         for v in vals:
@@ -172,6 +230,8 @@ class TestPlotFunctions(unittest.TestCase):
         print('In tearDown()')
         del self.xdata
         del self.ydata
+        del self.ydata_wco2
+        del self.xdata_wco2
 
 if __name__ == '__main__':
     unittest.main()
